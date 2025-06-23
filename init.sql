92802cf9af5c5009ca329767d6b42512ef4a0dd3
garnsev-dmitri <daimon1220033@yandex.ru>
2025-06-23 10:12:32 +0300

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS todolist;
+
+USE todolist;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    telegram_id VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    text VARCHAR(255) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
